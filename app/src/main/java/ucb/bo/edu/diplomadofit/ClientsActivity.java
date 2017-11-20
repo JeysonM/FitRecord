1d56de4ad3817b3cd63b3d4280a783b8908f7c0b
¨JeysonMO¨ <jeyson.mirabal@gmail.com>
2017-11-20 07:05:01 -0400

    ¨¨doc,fotos,apk¨

--- app/src/main/java/ucb/bo/edu/diplomadofit/ClientsActivity.java
+++ app/src/main/java/ucb/bo/edu/diplomadofit/ClientsActivity.java
@@ -1,14 +1,8 @@
 package ucb.bo.edu.diplomadofit;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -21,7 +15,6 @@ import io.realm.RealmResults;
 public class ClientsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Realm myRealm;
-    private Integer clientCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +31,6 @@ public class ClientsActivity extends AppCompatActivity {
             RealmResults<Client> list = this.myRealm.where(Client.class).findAll();
             for (Client u : list) {
                 lista.add(new ClientofList(u.getId(),u.getName(),u.getEdad(),u.getEstatura(),u.getPeso(),u.getGrasa(),u.getImc(),u.getTelefono()));
-                clientCount++;
-                if(clientCount == 5){
-                    notificacionInterna();
-                }
             }
             //lista.add(new ClientofList("sdasd","Jose",22,33f,55f,33f,55f));
             ClientListAdapter userListAdapter = new ClientListAdapter(lista);
@@ -58,28 +47,5 @@ public class ClientsActivity extends AppCompatActivity {
     public void clickAdd(View view) {
         Intent Registrar = new Intent(getApplicationContext(), WelcomeActivity.class);
         startActivity(Registrar);
-    }
-
-
-
-    public void notificacionInterna() {
-        Intent i = new Intent(this, ClientsActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_ONE_SHOT);
-
-        Uri sonido = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-        android.support.v4.app.NotificationCompat.Builder notification = new NotificationCompat.Builder(this)
-
-                .setContentTitle("FitRecord")
-                .setContentText("Felicidades tienes 5 clientes Registrados!")
-                .setSound(sonido)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                ;
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notification.build());
-
-
     }
 }
